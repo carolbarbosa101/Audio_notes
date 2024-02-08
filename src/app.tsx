60994d2d70e6293c65f9ef4e5ebb3fa8079e8f57
carolbarbosa101 <cmp.1a.caroline@gmail.com>
2024-02-08 01:13:54 -0300

    segundo commit

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,4 @@
-export function App() {
-  return <h1>Hello World</h1>
-  }
+export function App() { // componentes funções em que a primeira letra é maiscula, que devolvem HTML
+  return <h1>Hello World</h1> // a partir do momento que o html é utilizado dentro do tsx ele se torna JSX e não mais HTML
+  } // é só uma representação de html 
+  
