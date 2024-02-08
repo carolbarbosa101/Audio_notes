4b2661a1a543b3d2b251bd8a3688b0b6fd927ad8
carolbarbosa101 <cmp.1a.caroline@gmail.com>
2024-02-08 01:45:35 -0300

    commit

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,15 @@
+import React from "react";
+
 export function App() { // componentes funções em que a primeira letra é maiscula, que devolvem HTML
-  return <h1>Hello World</h1> // a partir do momento que o html é utilizado dentro do tsx ele se torna JSX e não mais HTML
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  ) // a partir do momento que o html é utilizado dentro do tsx ele se torna JSX e não mais HTML
   } // é só uma representação de html 
-  
+
+
+
+
+  /* tailwind css tem a vantagem de ser padronizado, sua aplicação do
+   inicio ao fim, nao importa qual dev codifica a aplicação */
